a2447334418e69e7c7de28c7ad0b72b53f85f459
Serhii Kushnir <s1983kushnir@gmail.com>
2026-01-17 21:57:39 +0100

    Remove index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
-import { GameRoomManager } from "../GameRoom";
+import { GameRoomManager } from "./GameRoom";
 
 const app = express();
 const httpServer = createServer(app);
